16cb402fa3f5401862d024124ac0fd8403ccdd5c
Thinh Le <thinhle@Thinhs-MacBook-Pro.local>
2023-09-25 14:30:37 +0700

    fix test case encoder

--- backend/api-gateway/common/utils/encoder_test.go
+++ backend/api-gateway/common/utils/encoder_test.go
@@ -11,21 +11,19 @@ func TestEncodeBase62(t *testing.T) {
 	result := EncodeBase62(0)
 	assert.Equal(t, "", result)
 
-	// Test case 2: Input 10 should result in "a" in base62
+	// Test case 1: Input 10 should result in "a" in base62
 	result = EncodeBase62(10)
 	assert.Equal(t, "a", result)
 
-	// Test case 3: Input 61 should result in "Z" in base62
+	// Test case 2: Input 61 should result in "Z" in base62
 	result = EncodeBase62(61)
 	assert.Equal(t, "Z", result)
 
-	// Test case 4: Input 62 should result in "10" in base62
+	// Test case 3: Input 62 should result in "10" in base62
 	result = EncodeBase62(62)
 	assert.Equal(t, "10", result)
 
-	// Test case 5: Input 100 should result in "1C" in base62
+	// Test case 4: Input 100 should result in "1C" in base62
 	result = EncodeBase62(100)
 	assert.Equal(t, "1C", result)
-
-	// Add more test cases for different inputs and expected outputs
 }
